16cfac2bf92cb26f70e5d74749290fb666440743
tarianwei <69235245+tarianwei@users.noreply.github.com>
2020-08-17 08:07:53 +0800

    Update index.js

--- src/jd/blindBox/index.js
+++ src/jd/blindBox/index.js
@@ -77,7 +77,6 @@ async function main(cookie) {
     return doActivePath(taskCoin.name, {type}).then(successLog);
   }
 
-/*
   async function lottery() {
     await sleep();
     await doPrizePath(lottery.name).then(async data => {
@@ -88,7 +87,7 @@ async function main(cookie) {
       }
     });
   }
-*/  
+
   function successLog({code = 200, coinNum}) {
     code === 200 && _printLog(`获取到的热力值为: ${coinNum}`);
   }
