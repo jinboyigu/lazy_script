fed519c06cc1d0ce904222539e8af3f8b2a0268d
jyhuang <326127285@qq.com>
2023-06-19 10:20:52 +0800

    perf(shell/updateNewEnvByCharles): 增加小程序 cookie 的更新逻辑

--- src/shell/updateNewEnvByCharles/index.js
+++ src/shell/updateNewEnvByCharles/index.js
@@ -7,20 +7,25 @@ function updateNewEnvByCharles() {
   const {JD_COOKIE_OPTION} = readFileJSON('../../../.env.product.json', __dirname);
   if (!JD_COOKIE_OPTION) return console.log('无需更新');
   const result = readDirJSON(__dirname);
-  const ptPin = 'pt_pin';
-  const newCookies = _.uniqBy(_.filter(result.map(o => {
+  const cookieKeys = [
+    ['wq_uin', 'wq_skey'],
+    ['pt_pin', 'pt_key'],
+  ];
+  const newCookies = _.filter(result.map(o => {
     const cookie = new Cookie('');
     o.request.header.headers.forEach(({name, value}) => {
       if (name === 'cookie') {
         cookie.add(value);
       }
     });
-    return _.pick(cookie.toObject(), [ptPin, 'pt_key']);
-  }), v => !_.isEmpty(v)), ptPin);
+    const targetKeys = cookieKeys.find(keys => cookie.get(keys[0]));
+    if (!targetKeys) return;
+    return _.pick(cookie.toObject(), targetKeys);
+  }), v => !_.isEmpty(v));
   const jdCookieOption = JD_COOKIE_OPTION.map(o => {
     const cookies = o.cookies;
-    const target = newCookies.find(o => o[ptPin] === cookies[ptPin]) || {};
-    return {cookies: target};
+    const target = newCookies.filter(o => cookieKeys.some(([pinKey]) => o[pinKey] === cookies[pinKey])) || [];
+    return {cookies: _.merge({}, ...target)};
   });
   const data = {JD_COOKIE_OPTION: jdCookieOption};
   writeFileJSON(data, '../../../.env.new.json', __dirname);
