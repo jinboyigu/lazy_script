29c9b056d0a674e85786c55e3f8b7e5cb421042f
jyhuang <326127285@qq.com>
2023-09-04 09:37:34 +0800

    update(shell/updateShopSignFromMail.js): 调整逻辑

--- src/shell/updateShopSignFromMail.js
+++ src/shell/updateShopSignFromMail.js
@@ -10,9 +10,12 @@ const {getMoment} = require('../lib/moment');
 const {updateProcessEnv} = require('../lib/env');
 const {exec} = require('../lib/common');
 
-async function updateShopSignFromMail() {
-  const since = getMoment().subtract(2, 'day');
-  const text = await search({since}).then(messages => _.map(messages, 'text').join('\n'));
+const [nodePath, filePath, day] = process.argv;
+
+async function updateShopSignFromMail(day = 2) {
+  const since = getMoment().subtract(day, 'day');
+  const text = await search({since}).then(messages => _.map(_.filter(messages, o => !o.subject.startsWith('lazy_script_new_env')), 'text').join('\n'));
+  fs.writeFileSync(path.resolve(__dirname, '../jd/wq/shopGift.url'), '');
   if (!text) return console.log(`没有找到相应数据(${since.formatDate()}~至今)`);
   console.log(`开始从邮件内容中更新 shopGift.url`);
   fs.writeFileSync(path.resolve(__dirname, '../jd/wq/shopGift.url'), text);
@@ -20,4 +23,4 @@ async function updateShopSignFromMail() {
 }
 
 updateProcessEnv();
-updateShopSignFromMail();
+updateShopSignFromMail(_.isNil(day) ? day : +day);
