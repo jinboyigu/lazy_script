c9291d0c48ce8cbd3f0173eaa10636ca412ebd8b
jyhuang <326127285@qq.com>
2023-10-24 11:35:09 +0800

    update(jd/FirePower): 更新活动逻辑

--- src/jd/618/FirePower.js
+++ src/jd/618/FirePower.js
@@ -13,37 +13,45 @@ class FirePower extends Template {
   static times = 1;
   static needInAppComplete1 = true;
   static commonParamFn = () => ({
-    appid: 'u',
+    appid: 'u_hongbao',
     loginType: 2,
     client: 'apple',
-    clientVersion: '12.0.2',
+    clientVersion: '12.1.6',
   });
-  static activityEndTime = '2023-06-18';
+  static activityEndTime = '2023-11-11';
   static concurrent = () => this._command[0];
 
   static apiOptions() {
-    return {};
+    return {
+      options: {
+        headers: {
+          origin: 'https://pro.m.jd.com',
+          referer: 'https://pro.m.jd.com/mall/active/39WF8DJPB4DByGz4Ly9EHeKvJz54/index.html?unionActId=31165&abPageType=1&uabt=305_2082_1-302_2196_1_1&d=0ivOeC8&s=&cu=true&utm_source=kong&utm_medium=jingfen&utm_campaign=t_1003272801_&utm_term=5df8d73acec741c1aabb841f2e07adb5',
+        },
+      },
+    };
   }
 
   static async beforeRequest(api) {
     const self = this;
 
-    api.cookieInstance.set('CSID', 'QTw9HCRRUlEBQloMWUdYTlFmdSlwflNaF1gDABcMDFVqe3Z7d3hxchVWVS1TWlRWWndgYAxeRBVnZBlzXF9aOkBSQFxCWllZYXJmdnlX');
-    // await api.doGetUrl('https://u.jd.com/OzlWBuX', {
+    // await api.doGetUrl('https://u.jd.com/0ivOeC8', {
     //   setCookieKeys: ['CSID'],
     // });
-    await api.doGetUrl('https://u.jd.com/jda?e=618%7Cpc%7C&p=JF8BAQIJK1olXDYDZBoCUBVIMzZNXhpXVhgcFR0DXR9QHDMXQA4KD1heSgINVRYJUz1NQxxBQwRdKwICTTRURzhjQAF-KgZrLyUnUhRiRx1bSzsLBVlXABdCUQ5LXmFMRANLAnZQESYIBEkXA2gLK15UDXB2Eh4tajhHUxBRSxwdPQICKS5RBHsSA24JH18TWAUEZF5cCUkUBWwBGlslbQYBZBUzCXsVA24JG1wWXAIBZF5aAU8UBWwKEl4TVAUyU15UOJ6Yr7qHt1kWWQIBVm5tCksXBGw4GGsSXQ8WUiwcWl8RcV84G1wlXjYyVl9cDEInM3ZqbjpDPQVWESMVWAAfXW9rA2s&a=fCg9UgoiAwwHO1BcXkQYFFljfn51cl5eQl0zVRBSUll%2bAQAPDSwjLw%3d%3d&refer=norefer&d=OzlWBuX&h5st=-1917063884', {
+    // CSID 需从 charles 中获取, 不然可能会无效
+    api.cookieInstance.set('CSID', 'RG1qGCNQWAtcQQBcWU9cQlAwdHl9LF8NFQtUBRFZWwVqe3Z7d3hxchVWVS1TWlRWWnZmYA5SRBRvZBhzXFldOkdRXkNHV0BGYHVmdXtS');
+    await api.doGetUrl('https://u.jd.com/jda?e=99_1|1_28|||&p=JF8BAPEJK1olXDYDZBoCUBVIMzZNXhpXVhgcFR0DFxcIWDoXSQVJQ1pSCQNDWBlSWyhcBV5pVXRBMgkieiwVYApUaQhGPVVECFw1YCBcRAYWQwRACU8dDRsBVUVTXDdWRCtHGH5XWFxdCEwTMytURRNXJ2d2MT1HTDBgGSthbDtrD0AOWG5cOEsWAm0LHVgcXAYyZF5ZOAB5Al8JK1sSVQAKXFxfDksSB2g4G10liImugdHxCkgTB2wKK2sXXQYFUG5eOEMfbW9FGzUWVEtPGW5tCEwnAF84GFoUXAYyZIDQuC5iYTULaA8WBnBlFjwDcUnJjt84&a=fCg9UgoiAwwHO1BcXkQYFFljf3B1e1FWQFozVRBSUll%2bAQAPDSwjLw%3d%3d&refer=norefer&d=0ivOeC8&h5st=952551976', {
       headers: {
-        referer: 'https://u.jd.com/OzlWBuX',
+        referer: 'https://u.jd.com/0ivOeC8',
       },
       setCookieKeys: ['unpl'],
     });
 
     self.injectEncryptH5st(api, {
       config: {
-        queryFullGroupInfoMap: {appId: '6a98d'},
-        doInteractiveAssignment: {appId: '6a98d'},
-        getCoupons: {appId: '6a98d'},
+        queryFullGroupInfoMap: {appId: '7b74b'},
+        doInteractiveAssignment: {appId: '7b74b'},
+        getCoupons: {appId: 'c822a'},
       },
       signFromSecurity: true,
     });
@@ -52,8 +60,8 @@ class FirePower extends Template {
   static async doMain(api, shareCodes) {
     const self = this;
 
-    const unionActId = '31162';
-    const actId = '3nNmntNrufZjkZF1XJJKknDuCbaQ';
+    const unionActId = '31165';
+    const actId = '27F8qXYtc6pi1sjybdSavaLjSvBL';
     const d = ''; // 邀请码
 
     const getCouponCronHour = _.first(self._command);
@@ -88,6 +96,7 @@ class FirePower extends Template {
 
       for (const {projectId: encryptProjectId, taskId: encryptAssignmentId, status, showInfo} of groupInfo) {
         if (status !== 1 || !encryptAssignmentId) continue;
+        if (showInfo.match('分享活动抽盲盒')) continue; // 不需要执行的任务(基本无收益)
         const body = {
           encryptProjectId,
           encryptAssignmentId,
@@ -116,18 +125,24 @@ class FirePower extends Template {
     async function getCoupons(unionShareId = '') {
       const body = {
         actId, unionActId, d,
-        'platform': 2,
+        'platform': 1,
         unionShareId,
       };
+      const options = {
+        headers: {
+          'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.42(0x18002a2e) NetType/WIFI Language/zh_CN',
+        },
+      };
       // 先检查是否有次数
-      const {code, msg} = await api.doFormBody('showCoupon', body);
+      const {code, msg} = await api.doFormBody('showCoupon', body, void 0, options);
       if (code !== 0) {
+        api.clog('可能是 CSID 失效了')
         return api.clog(msg);
       }
       await api.doFormBody('getCoupons', {
         ...body,
         'type': 1,
-      }).then(data => {
+      }, void 0, options).then(data => {
         const coupon = _.get(data, 'data.couponList[0]');
         if (!coupon) return api.clog(data.msg);
         const typeLabel = ['', '红包', '', '优惠券'];
