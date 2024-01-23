162fd2d6200a5e1f33a855bd5bbd19a399173175
jyhuang <326127285@qq.com>
2024-01-23 10:18:29 +0800

    update(shell/updateEnvFromMail): 增加判断是否需要更新逻辑

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const MailParser = require('mailparser').MailParser;
-const {getEnv, updateProductEnv} = require('./env');
+const {getEnv, updateProductEnv, getProductEnv} = require('./env');
 const {getMoment} = require('./moment');
 const {readFileJSON, writeFileJSON, formatJSONOutput} = require('./common');
 const Imap = require('imap');
@@ -215,9 +215,13 @@ async function updateEnvFromMail(day = 7) {
     }));
   const allNewEnvs = await getNewEnvs();
   if (_.isEmpty(allNewEnvs)) return console.log(`没有找到相应数据(${nowMoment.formatDate()}~至今)`);
-  // TODO 判断是否需要更新
   const newEnv = _.merge({}, ...allNewEnvs.reverse());
-  console.log(`开始从邮件内容中更新 new env`);
+  const oldProductEnv = getProductEnv();
+  const newProductEnv = _.merge({}, oldProductEnv, newEnv);
+  if (JSON.stringify(oldProductEnv) === JSON.stringify(newProductEnv)) {
+    return console.log('[updateEnvFromMail] 数据一致, env 不需要更新');
+  }
+  console.log(`[updateEnvFromMail] 开始从邮件内容中更新`);
   console.log(formatJSONOutput(newEnv));
   updateProductEnv(newEnv, false, true);
 }
