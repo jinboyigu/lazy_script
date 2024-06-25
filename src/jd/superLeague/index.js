b5c1b7be11b0305af94dc36639e08f7359865a96
jyhuang <326127285@qq.com>
2024-06-25 17:36:14 +0800

    update(src/jd/superLeague): 更新逻辑

--- src/jd/superLeague/index.js
+++ src/jd/superLeague/index.js
@@ -1,8 +1,10 @@
 const Template = require('../base/template');
 
-const {sleep, writeFileJSON, singleRun, replaceObjectMethod} = require('../../lib/common');
+const {sleep, writeFileJSON, singleRun, replaceObjectMethod, matchMiddle} = require('../../lib/common');
 const {getMoment} = require('../../lib/moment');
 const _ = require('lodash');
+
+let linkId = '';
 
 class SuperLeague extends Template {
   static scriptName = 'SuperLeague';
@@ -10,7 +12,7 @@ class SuperLeague extends Template {
   static dirname = __dirname;
   static shareCodeTaskList = [];
   static commonParamFn = () => ({
-    body: {'linkId': 'kKf0fO1O_28HF2Ff0hucCw'},
+    body: {linkId},
     appid: 'activities_platform',
     client: 'ios',
     clientVersion: '13.1.0',
@@ -19,6 +21,7 @@ class SuperLeague extends Template {
   static times = 1;
   static needInAppDynamicTime = true;
   static needOriginProMd = true;
+
   static activityEndTime = '2024-06-20';
 
   static apiOptions() {
@@ -27,6 +30,13 @@ class SuperLeague extends Template {
 
   static async beforeRequest(api) {
     const self = this;
+
+    const originUrl = `https://pro.m.jd.com/mall/active/2uXu6yffAWogkJDsRkxFq5YiCfGT/index.html?stath=47&navh=44&inviter=vNsp7eclDGU79VZWwzuDLw&utm_term=c03ff960a30148359a9978cb86752c61&utm_source=kong&tttparams=IYIcwUeyJnTGF0IjoiMjIuOTQyOTA2Iiwic2NhbGUiOiIzIiwidW5fYXJlYSI6IjE5XzE2MDFfMzY5NTNfNTA0MDIiLCJkTGF0IjoiIiwid2lkdGgiOiIxMTcwIiwicHJzdGF0ZSI6IjAiLCJhZGRyZXNzSWQiOiI5NDIxMTMyNzE4IiwibGF0IjoiMC4wMDAwMDAiLCJwb3NMYXQiOiIyMi45NDI5MDYiLCJwb3NMbmciOiIxMTMuNDc0ODAxIiwiZ3BzX2FyZWEiOiIwXzBfMF8wIiwibG5nIjoiMC4wMDAwMDAiLCJ1ZW1wcyI6IjAtMC0wIiwiZ0xuZyI6IjExMy40NzQ4MDEiLCJtb2RlbCI6ImlQaG9uZTEzLDMiLCJkTG5nIjoiIn60%3D&utm_medium=jingfen&cu=true&femobile=femobile&utm_campaign=t_1003272801_&__fr=deeplink&taskId=5101&activityChannel=jdapp`;
+    await api.doGetUrl(originUrl).then((data => {
+      const newLinkId = matchMiddle(data, {reg: /"linkId":"([^"']*)"/});
+      if (newLinkId === linkId) return;
+      linkId = newLinkId;
+    }));
 
     self.injectEncryptH5st(api, {
       config: {
@@ -43,18 +53,20 @@ class SuperLeague extends Template {
 
     await self.beforeRequest(api);
 
-    await handleDoTask();
-
-    const {remainTimes} = await api.doFormBody('superLeagueHome').then(_.property('data'));
-    for (let i = 0; i < remainTimes; i++) {
-      await api.doFormBody('superLeagueLottery').then(data => {
-        api.log(`抽奖获得 ${_.get(data, 'data.codeDesc') || '空气'}(${_.get(data, 'data.prizeDesc')})`);
-      });
-      await sleep(2);
+    if (!linkId) {
+      return api.log('没找到 linkId');
     }
+
+    const stop = await handleDoTask();
+
+    if (stop) {
+      return api.log(`linkId(${linkId})无效`);
+    }
+    await handleLottery();
 
     async function handleDoTask() {
       const taskList = await api.doFormBody('apTaskList').then(_.property('data'));
+      if (!taskList) return true;
       for (const {
         id: taskId,
         taskType,
@@ -62,15 +74,15 @@ class SuperLeague extends Template {
         taskLimitTimes: maxTimes,
         taskDoTimes: times,
         taskFinished
-      } of taskList) {
+      } of taskList || []) {
         if (taskFinished || /助力/.test(taskTitle) || ['FOLLOW_CHANNEL'].includes(taskType)) continue;
         const channel = 4;
         const {taskItemList} = await api.doFormBody('apTaskDetail', {
           taskType,
           taskId,
           channel,
-        }).then(_.property('data'));
-        await self.loopCall(taskItemList, {
+        }).then(_.property('data')) || {};
+        taskItemList && await self.loopCall(taskItemList, {
           times,
           maxTimes,
           async firstFn({itemId}) {
@@ -78,6 +90,16 @@ class SuperLeague extends Template {
             return api.doFormBody('apsDoTask', {taskType, taskId, channel, itemId});
           },
         });
+      }
+    }
+
+    async function handleLottery() {
+      const {remainTimes} = await api.doFormBody('superLeagueHome').then(_.property('data'));
+      for (let i = 0; i < remainTimes; i++) {
+        await api.doFormBody('superLeagueLottery').then(data => {
+          api.log(`抽奖获得 ${_.get(data, 'data.codeDesc') || '空气'}(${_.get(data, 'data.prizeDesc')})`);
+        });
+        await sleep(2);
       }
     }
   }
