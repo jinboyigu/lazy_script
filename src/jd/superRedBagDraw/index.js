834dac05cb1a63287a8ad03b0b5234b204a16310
jyhuang <326127285@qq.com>
2024-07-18 17:50:23 +0800

    update(jd/superRedBagDraw): 调整触发逻辑

--- src/jd/superRedBagDraw/index.js
+++ src/jd/superRedBagDraw/index.js
@@ -15,14 +15,14 @@ class SuperRedBagDraw extends Template {
     body: {'linkId': 'aE-1vg6_no2csxgXFuv3Kg'},
     appid: 'activity_platform_se',
     client: 'ios',
-    clientVersion: '13.1.0',
+    clientVersion: '13.1.1',
     loginType: 2,
     loginWQBiz: 'wegame',
   });
-  static needInAppDynamicTime = true;
+  static needInApp = false;
   static keepIndependence = true;
   static times = 1;
-  static activityEndTime = '2024-06-20';
+  static activityEndTime = '2024-08-31';
   static needOriginProMd = true;
   static concurrent = true;
   static concurrentOnceDelay = 20;
@@ -40,12 +40,13 @@ class SuperRedBagDraw extends Template {
 
     self.injectEncryptH5st(api, {
       config: {
+        superRedBagHome: {appId: '5be1b'},
         superRedBagDraw: {appId: '89cfe'},
         // 本地加密会报错
         // superRedBagList: {appId: 'f2b1d'},
         apCashWithDraw: {appId: '73bca'},
       },
-      signFromSecurity: true,
+      signFromKEDAYA: true,
     });
   }
 
@@ -54,36 +55,24 @@ class SuperRedBagDraw extends Template {
 
     await self.beforeRequest(api);
     if (self.getNowHour() === 19) {
+      const nextLeftTime = await api.doFormBody('superRedBagHome').then(_.property('data.nextLeftTime'));
+      if (!nextLeftTime) {
+        api.logBoth('当前无活动');
+        return;
+      } else {
+        api.logBoth(`需等待 ${nextLeftTime} 后执行`);
+      }
       if (api.currentCookieTimes === 0) {
         console.log(`等待19点30分后执行`);
       }
       await sleepTime([19, 30]);
       await superRedBagDraw();
     }
-    await handleExchange();
+    // TODO 提现暂关闭
+    // await handleExchange();
 
     // 1 分钟摇一摇
     async function superRedBagDraw(isContinue = true) {
-      /* TODO 后续可能需要直接用原数据 */
-      const form = formatPasteData(`body\t{"linkId":"aE-1vg6_no2csxgXFuv3Kg"}
-t\t1718364936610
-appid\tactivity_platform_se
-client\tios
-clientVersion\t12.3.2
-loginType\t2
-loginWQBiz\twegame
-h5st\t20240614193536637;iiiyg5i5ztn695i2;89cfe;tk03wd1dd1d0418nuI7HLOyl3rlAmuxiFhOjBb2MW-hoeV2vwVOpzbY7gsVr8lsDs7TUuG5UENYgcEof0_PXolIZu3TJ;83d216700c589d9c0c69da201c2d450dfb48448d83a84c1f3130faa1132ff254;4.7;1718364936637;TKmWNRTOL-4LqGbElsxi8AxtCFkA0eUOcvyYjOgUq7vg2_K8UM9N0bRc_mLJ2x3vExSbzMxaZ-bMUzzi9KBrb80vxzSoPYA2g-g1I_URRIT4CLQBqyppVxWweLzPcAhlGELSD4GmkzhNPsWgGDSDhp0IXsqJB1Ye--mdn30Bn4XDWr-H9e7zICL09ojrUcrhqUaQsl82NlwF-5YUeinLw2cy6qX79LRm1Od6OuDtw8RPsXPfBcsUeixl-jnGegu1O4tcTYhqgh1wz5UywQ4fUmWoQREV8-GQMrATm0yd53b4CSlQRFH3BxqCSreR1mAQlzAei3u-sIcjhORf9ODKHLBad5zBaWWsupYJ3sCn2jCh1P0M9JPFWwd1vKxN8QKg1ZWgOXsXz3QNtdyarVCEuhU6dsqe3KzB7K89QdjAvxWa1hwGxzRNDtBwYXJoTMRJ0YDA
-uuid\t072a16472e038821457fa13a510fcbcd014faf77
-build\t169031
-screen\t375*667
-networkType\twifi
-d_brand\tiPhone
-d_model\tiPhone8,1
-lang\tzh_CN
-osVersion\t12.2
-partner\t-1
-x-api-eid-token\tjdd03KCWOTOKUUPOLJLBDBBU6UKQPIGXTIKYQNA5P5K7CHWY2XEYBA4SZN4IMQISKUH27F52QIYXRUNG6K6QSAZKW4JXPJIAAAAMQC2DMTLYAAAAADQH27YLMV3HTKUX
-cthr\t1`);
       const data = await api.doFormBody('superRedBagDraw');
       if (!self.isSuccess(data)) {
         api.log(data.errMsg);
@@ -112,25 +101,6 @@ cthr\t1`);
         'pageSize': 20,
         'associateLinkId': '',
         'business': 'superRedEnvelope',
-      }, void 0, {
-        form: formatPasteData(`body\t{"pageNum":1,"pageSize":20,"linkId":"aE-1vg6_no2csxgXFuv3Kg","associateLinkId":"","business":"superRedEnvelope"}
-t\t1718588808759
-appid\tactivities_platform
-client\tios
-clientVersion\t13.1.0
-loginType\t2
-loginWQBiz\twegame
-h5st\t20240617094648772;ii6in6yynt5gm9i1;f2b1d;tk03wa6f21c3c18n8VAbOXAu5Oy7_omzPIWqKPQ9TYfu93H2Op1UxYH5VhzzLmyCeelp5wPmikxX83JB0YHQcMZnB6vT;127854b515bd63c93d7faa2f6c974d33983bd60356125689c806ac254c602d77;4.7;1718588808772;TKmWWU77SPrM7P7ZkH7Pt6mifb50MrYQeAsfO8mRikaao-8cv3UAjqVMC6DAxjrSEDS_UHNEh-5-A_cT8rRBRsMOooyV-Ntr4viJyp-e1lpS6p1WkJRn9f3zLczQ_6X-SxXPxJoWz9Vz3t87-u-rkyB8EIseUn6rhQHp7KP-Vh-RQV6Te7D456ncoUsyEFOCdsoCbc7Cq1J4lSGYYFVBRkReVbyGe_SpfyqTE7O137fCDOd4jdSyzitNA3fn_ub9STqYJWBo8cw6-OOGHKEmMbskbzIi2Bgukq3TmIriXaRfeVEFY56zu7Fjl4ZkPLGM8S6R7L550u5kLx7r4PXptP-dOwHVFNO6oWLdYuOPR_R-P9v1LflBdE4XEXqYkUY91M_Ew4NDnb9VPZGA3fCwNuH2ktqe3KzB7K89QdjAvxWa1hwGxzRNDtBwYXJoTMRJ0YDA
-uuid\tc6993893af46e44aa14818543914768cf2509fbf
-build\t169370
-screen\t390*844
-networkType\twifi
-d_brand\tiPhone
-d_model\tiPhone13,3
-lang\tzh_CN
-osVersion\t17.5
-partner\t-1
-cthr\t1`),
       }).then(data => _.get(data, 'data.items', []).find(o => o.prizeType === 4 && o.state === 0));
       if (prizeData) {
         await api.doFormBody('apCashWithDraw', {
