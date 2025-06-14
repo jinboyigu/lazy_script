62ad7bafd6d2263e0f3202fc92e493aad7df9b93
jyhuang <326127285@qq.com>
2025-06-14 21:43:30 +0800

    update: 不需要自动转换

--- src/jd/base/index.js
+++ src/jd/base/index.js
@@ -553,7 +553,7 @@ class Base {
       };
       if (self.needChangeCK && processInAC() && !changedCK[ptPin] && false) {
         changedCK[ptPin] = 1;
-        await self.changeCK(api, !processInAC() && [7, 10, 14, 18, 22].includes(getNowHour()));
+        //await self.changeCK(api, !processInAC() && [7, 10, 14, 18, 22].includes(getNowHour()));
       }
       // 停止运行该脚本
       if (disabled) return;
