49f2913c4d92d325cbb79d150b0d7268a5d3e69b
jyhuang <326127285@qq.com>
2025-06-16 17:44:45 +0800

    update: 调整脚本

--- src/runHour.js
+++ src/runHour.js
@@ -1,5 +1,5 @@
 /* 执行某个时间段的 app.js */
 global.isRunHour = true;
-const [nodePath, filePath, hour = 0] = process.argv;
+const [nodePath, filePath, hour = 0, target = 'others'] = process.argv;
 require('./lib/moment').setNowHour(+hour);
-require('./app');
+require(`./${target}`);
