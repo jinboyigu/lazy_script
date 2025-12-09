e108da3cdadfbabe810bddd2c08f7e51aa14482c
jyhuang <326127285@qq.com>
2025-12-09 09:40:52 +0800

    update: 移除GitHub action

--- src/local.js
+++ src/local.js
@@ -5,26 +5,28 @@
  */
 
 require('./appBase')([
-  [[9, 15, 20, 22], require('./jd/local/ChangeCK')],
+  [[5, 9, 15, 20, 22], require('./jd/local/ChangeCK')],
+  [['*'], require('./jd/plantBean'), 0, {isCron: true}],
   // 实时执行
   // hour0
   ...[
-    require('./jd/bSign'),
+    // require('./jd/bSign'),
+    require('./jd/newtry'),
   ].map(target => [[0], target]),
   // [['*'], require('./jd/fruit/1Wheels')],
   [[0, 4, 8, 9, 12, 20, 22], require('./jd/joy')],
   [[1, 7, 18], require('./jd/dwapp/PointsGasStation')],
   [[7, 15, 21], require('./jd/plantBean')],
-  [[7, 21], require('./jd/sign')],
+  // [[7, 21], require('./jd/sign')],
   // [[10, 12, 23], require('./jd/turnHappy/WanYiWan')],
-  [[0, 10, 20], require('./jd/lite/JoyPark'), {weekday: 1}],
-  [[0], require('./jd/618/FirePower')],
+  // [[0, 10, 20], require('./jd/lite/JoyPark'), {weekday: 1}],
+  // [[0], require('./jd/618/FirePower')],
   // [[7, 12, 18, 20, 22], require('./jd/fruit/1')],
   // [[20], require('./jd/applet/hbrain')],
-  [[7, 18], require('./jd/purchaseCard')],
+  // [[7, 18], require('./jd/purchaseCard')],
   // [[8], require('./jd/applet/mini')],
   // 定时任务
-  [[19], require('./jd/superRedBagDraw'), 25],
+  // [[19], require('./jd/superRedBagDraw'), 25],
   // [[23, 10, 22], require('./jd/fission'), 32],
   [[23, 7, 15], require('./jd/joy/redeem'), 54],
   // [[23], require('./jd/turnHappy/WanYiWan'), 60],
--- src/shell/updateActionEnv.js
+++ src/shell/updateActionEnv.js
@@ -89,4 +89,5 @@ async function main() {
   }
 }
 
-main();
+// main();
+console.log('github action 已下线');
